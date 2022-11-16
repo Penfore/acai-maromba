359a35a1b550cb607cc733927d3092852c5cf9f9
Fulvio Leo <fulviokaldas@outlook.com>
2022-11-15 21:49:07 -0300

    binding da pagina de relatorios

--- src/controllers/relatorioController.java
+++ src/controllers/relatorioController.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import javax.swing.text.TableView;
+// import javafx.scene.control.TableView;
+// import javafx.fxml.FXML;
+
+import model.entities.Relatorio;
 import view.Telas;
 
 public class relatorioController {
+	@FXML
+	private TableView<Relatorio> listarRelatoriosTable;
 
 	public void voltar() {
 		Telas.telaHome();
 	}
 
+	@FXML
 	public void listar() {
 
 	}

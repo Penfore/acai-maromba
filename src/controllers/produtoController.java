c8d907fa85ecc611d07f3f9a91d8c32869f9f76f
Fulvio Leo <fulviokaldas@outlook.com>
2022-11-15 21:47:18 -0300

    binding da tela de produtos

--- src/controllers/produtoController.java
+++ src/controllers/produtoController.java
@@ -1,17 +1,34 @@
 package controllers;
 
+import javax.swing.text.TableView;
+
+import model.entities.Produto;
+// import javafx.scene.control.TableView;
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Button;
 import view.Telas;
 
 public class produtoController {
+	@FXML
+	private TableView<Produto> listarProdutosTable;
+	@FXML
+	private Button adicionarProdutoButton;
+	@FXML
+	private Button alterarProdutoButton;
+	@FXML
+	private Button deletarProdutoButton;
 
+	@FXML
 	public void adicionarProduto() {
 
 	}
 
+	@FXML
 	public void alterar() {
 
 	}
 
+	@FXML
 	public void deletar() {
 
 	}
@@ -20,6 +37,7 @@ public class produtoController {
 		Telas.telaHome();
 	}
 
+	@FXML
 	public void listar() {
 
 	}

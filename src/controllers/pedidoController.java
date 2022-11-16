5d7626b0e3315d4e70019da3402329c733857626
Fulvio Leo <fulviokaldas@outlook.com>
2022-11-15 21:41:33 -0300

    binding da tela de pedidos

--- src/controllers/pedidoController.java
+++ src/controllers/pedidoController.java
@@ -1,17 +1,34 @@
 package controllers;
 
+import javax.swing.text.TableView;
+// import javafx.scene.control.TableView;
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Button;
+
+import model.entities.Pedido;
 import view.Telas;
 
 public class pedidoController {
+	@FXML
+	private TableView<Pedido> listarPedidoTable;
+	@FXML
+	private Button adicionarPedido;
+	@FXML
+	private Button alterarPedidoButton;
+	@FXML
+	private Button deletarPedidoButton;
 
+	@FXML
 	public void adicionarPedido() {
 
 	}
 
+	@FXML
 	public void alterar() {
 
 	}
 
+	@FXML
 	public void deletar() {
 
 	}
@@ -20,6 +37,7 @@ public class pedidoController {
 		Telas.telaHome();
 	}
 
+	@FXML
 	public void listar() {
 
 	}

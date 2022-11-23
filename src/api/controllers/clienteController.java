31496254f2e1cb48f308ba6a4f9b3419c873a15a
edu-doc <antonioeduardooc@gmail.com>
2022-11-23 07:30:57 -0300

    Update Classes Controller

--- src/api/controllers/clienteController.java
+++ src/api/controllers/clienteController.java
@@ -2,14 +2,25 @@ package api.controllers;
 
 
 
-import model.entities.Cliente;
-import view.Telas;
 import api.dto.clienteDTO;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Services.ClienteBO;
+import model.entities.Cliente;
+import view.Telas;
 
 public class clienteController {
+	
+	@FXML private TextField nome;
+	@FXML private TextField telefone;
+	@FXML private TextField pais;
+	@FXML private TextField estado;
+	@FXML private TextField cidade;
+	@FXML private TextField logradouro;
+	@FXML private TextField numero;
+		  private ClienteBO bo = new ClienteBO();
 	
 	@FXML
 	private TableView<Cliente> listarTable;
@@ -20,7 +31,15 @@ public class clienteController {
 
 	@FXML
 	public void adicionarCliente() {
-
+		clienteDTO dto = new clienteDTO();
+		dto.setNome(nome.getText());
+		dto.setTelefone(telefone.getText());
+		dto.setPais(pais.getText());
+		dto.setEstado(estado.getText());
+		dto.setCidade(cidade.getText());
+		dto.setLogradouro(logradouro.getText());
+		dto.setNumero(numero.getText());
+		bo.adicionar(dto);
 	}
 
 	// TODO: Implementar o restante na view
--- src/api/dto/clienteDTO.java
+++ src/api/dto/clienteDTO.java
@@ -2,20 +2,18 @@ package api.dto;
 
 public class clienteDTO {
 	private String nome;
-	private String endereco;
 	private String telefone;
-
+	private String pais;
+	private String estado;
+	private String cidade;
+	private String logradouro;
+	private String numero;
+	
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
 	}
 	public String getTelefone() {
 		return telefone;
@@ -23,5 +21,38 @@ public class clienteDTO {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+
+	
 
 }
